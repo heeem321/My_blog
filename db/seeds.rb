afdbe8c59a82d8c6a962f2f5a0fbbe015be83ba0
Ebrahim <heeem321@hotmail.com>
2018-06-19 14:13:42 +0800

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create(name: 'Nos Expressions')
+Category.create(name: 'En Socite')
+Category.create(name: 'A Table')
+Category.create(name: 'Cul-Ture')
