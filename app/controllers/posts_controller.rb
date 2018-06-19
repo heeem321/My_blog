6cf0cda15cf793bc4e8278dc295fb71d0ad14f52
Ebrahim <heeem321@hotmail.com>
2018-06-19 14:19:33 +0800

    wrote controller resources

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+
+  def destory
+  end
+
+private
+
+def post_params
+end
+
+def find_post
+end
+
+
 end
