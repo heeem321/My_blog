e8edbf33a7f6bde4e7c61539d3495478e890f3ab
Ebrahim <heeem321@hotmail.com>
2018-06-24 13:22:40 +0800

    minor fix to controller and route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
   before_action :find_post, only [:show, :edit, :update, :destroy]
-  
+
   def index
-    @posts = Post.all
+    if params[:category].blank?
+      @posts = Post.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @posts = Post.where(category_id: @category_id).order("created_at DESC")
+    end
   end
-  
+
   def show
   end
 
@@ -24,11 +29,11 @@ class PostsController < ApplicationController
 
   def edit
   end
-  
+
   def update
     if @post.update(post_params)
       redirect_to @post, notice: "Update Seuccessful"
-    else 
+    else
       render edit
     end
   end
