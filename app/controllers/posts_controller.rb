eb6378c7199781bfad7c0231f08d172fdeb3a82c
Ebrahim <heeem321@hotmail.com>
2018-06-24 13:47:59 +0800

    minor fix: controller, model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_post, only [:show, :edit, :update, :destroy]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
     if params[:category].blank?
